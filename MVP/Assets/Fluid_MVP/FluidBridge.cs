38e0f398ef7979f51b037043b0f066e0fe58f8ec
Gwendal Grison <gwendal.grison@laposte.net>
2026-06-01 15:15:34 +0200

    feat: basic implementation to dialog between c# and compute shader

--- MVP/Assets/Fluid_MVP/FluidBridge.cs
+++ MVP/Assets/Fluid_MVP/FluidBridge.cs
@@ -6,37 +6,80 @@ using Random = UnityEngine.Random;
 
 public class FluidBridge : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private ComputeShader pbfShader;
     [SerializeField] private VisualEffect vfxGraph;
-    [SerializeField] private int particleCount = 1000;
 
+    [Header("Simulation Settings")]
+    [SerializeField] private int particleCount = 1000;
+    [SerializeField] private float cellSize = 1.0f;
+    [SerializeField] private float smoothingRadius = 1.0f;
+    [SerializeField] private Vector2 gravity = new Vector2(0f, -9.81f);
+
+    // Kernels ID
+    private int kernelPredict;
+
+    // VRAM
     private GraphicsBuffer particleBuffer;
+    private GraphicsBuffer predictedPositionsBuffer;
+    private GraphicsBuffer hashesBuffer;
+    private GraphicsBuffer neighborBuffer;
+
+    private struct GPUHash { public uint hash; public uint pid; }
+    private struct GPUNeighborData { public int count; public unsafe fixed int indices[64]; }
+
+    private float fixedDeltaTime = 0.016f; // 60Hz
+    private float accumulator = 0f;
+
     private FluidParticle[] rawParticles;
 
     void Start()
     {
-        if (vfxGraph == null)
+        if (vfxGraph == null || pbfShader == null)
         {
-            throw new System.NullReferenceException($"[FluidBridge] VisualEffect reference is missing on {gameObject.name}! Did you forget to drag and drop it in the Inspector?");
+            throw new System.NullReferenceException($"[FluidBridge] VisualEffect or PBFShader reference is missing on {gameObject.name}! Did you forget to drag and drop it in the Inspector?");
         }
+        kernelPredict = pbfShader.FindKernel("predictPositions");
         rawParticles = new FluidParticle[particleCount];
         for (int i = 0; i < particleCount; i++) {
-            rawParticles[i].position = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+            rawParticles[i].position = new Vector2(Random.Range(-5f, 5f), Random.Range(2f, 7f));
             rawParticles[i].velocity = new Vector2(0f, 0f);
-            rawParticles[i].type = (i % 2 == 0) ? 0.0f : 1.0f;
         }
         int stride = Marshal.SizeOf(typeof(FluidParticle));
         particleBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, particleCount, stride); // equivalent to glGenBuffers() and glBindBuffer() in OpenGL
         particleBuffer.SetData(rawParticles); // pushes data to VRAM
+        predictedPositionsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, particleCount, Marshal.SizeOf(typeof(Vector2)));
+        hashesBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, particleCount, Marshal.SizeOf(typeof(GPUHash)));
+        neighborBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, particleCount, Marshal.SizeOf(typeof(GPUNeighborData)));
+
         vfxGraph.SetGraphicsBuffer("ParticleBuffer", particleBuffer);
         vfxGraph.SetInt("ParticleCount", particleCount);
     }
 
-    // Update will be handled by the GPU
+    void Update() {
+        accumulator += Time.deltaTime;
+        int threadGroupsX = Mathf.CeilToInt((float)particleCount / 64f);
+        while (accumulator >= fixedDeltaTime) {
+            pbfShader.SetInt("ParticleCount", particleCount);
+            pbfShader.SetInt("GridCellCount", 2048);
+            pbfShader.SetFloat("cellSize", cellSize);
+            pbfShader.SetFloat("dt", fixedDeltaTime);
+            pbfShader.SetFloat("smoothingRadius", smoothingRadius);
+            pbfShader.SetVector("gravity", gravity);
+
+            pbfShader.SetBuffer(kernelPredict, "Particles", particleBuffer);
+            pbfShader.SetBuffer(kernelPredict, "PredictedPositionsBuffer", predictedPositionsBuffer);
+            
+            pbfShader.Dispatch(kernelPredict, threadGroupsX, 1, 1);
+
+            accumulator -= fixedDeltaTime;
+        }
+    }
     
     void OnDestroy() {
-        if (particleBuffer != null) {
-            particleBuffer.Release();
-            particleBuffer = null;
-        }
+        if (particleBuffer != null) { particleBuffer.Release(); particleBuffer = null; }
+        if (predictedPositionsBuffer != null) { predictedPositionsBuffer.Release(); predictedPositionsBuffer = null; }
+        if (hashesBuffer != null) { hashesBuffer.Release(); hashesBuffer = null; }
+        if (neighborBuffer != null) { neighborBuffer.Release(); neighborBuffer = null; }
     }
 }
--- MVP/Assets/Fluid_MVP/FluidParticle.cs
+++ MVP/Assets/Fluid_MVP/FluidParticle.cs
@@ -9,5 +9,4 @@ public struct FluidParticle // using a struct because it's a value type (contigo
 {
     public Vector2 position;
     public Vector2 velocity;
-    public float type; // 0.0 = water, 1.0 = other (probably fire)
 }
