c85d6df8130802ee9145b749e695e03f04c5a5fe
Gwendal Grison <gwendal.grison@laposte.net>
2026-06-02 14:31:16 +0200

    feat: changed C# bridge to adapt to the current version of the compute shader

--- MVP/Assets/Fluid_MVP/FluidBridge.cs
+++ MVP/Assets/Fluid_MVP/FluidBridge.cs
@@ -12,21 +12,25 @@ public class FluidBridge : MonoBehaviour
 
     [Header("Simulation Settings")]
     [SerializeField] private int particleCount = 1000;
+    [SerializeField] private int gridCellCount = 2048;
+    [SerializeField] private int maxParticlesPerCell = 10;
     [SerializeField] private float cellSize = 1.0f;
     [SerializeField] private float smoothingRadius = 1.0f;
     [SerializeField] private Vector2 gravity = new Vector2(0f, -9.81f);
 
     // Kernels ID
+    private int kernelClear;
+    private int kernelBuild;
     private int kernelPredict;
+    private int kernelDensity;
 
     // VRAM
     private GraphicsBuffer particleBuffer;
     private GraphicsBuffer predictedPositionsBuffer;
-    private GraphicsBuffer hashesBuffer;
-    private GraphicsBuffer neighborBuffer;
+    private GraphicsBuffer lambdaBuffer;
 
-    private struct GPUHash { public uint hash; public uint pid; }
-    private struct GPUNeighborData { public int count; public unsafe fixed int indices[64]; }
+    private GraphicsBuffer particlesInCellBuffer;
+    private GraphicsBuffer nInCellBuffer;
 
     private float fixedDeltaTime = 0.016f; // 60Hz
     private float accumulator = 0f;
@@ -39,38 +43,72 @@ public class FluidBridge : MonoBehaviour
         {
             throw new System.NullReferenceException($"[FluidBridge] VisualEffect or PBFShader reference is missing on {gameObject.name}! Did you forget to drag and drop it in the Inspector?");
         }
+        
+        kernelClear = pbfShader.FindKernel("clearNeighbourGrid");
+        kernelBuild = pbfShader.FindKernel("buildNeighbourGrid");
         kernelPredict = pbfShader.FindKernel("predictPositions");
+        kernelDensity = pbfShader.FindKernel("calculateDensityAndLambda");        
+        
         rawParticles = new FluidParticle[particleCount];
         for (int i = 0; i < particleCount; i++) {
             rawParticles[i].position = new Vector2(Random.Range(-5f, 5f), Random.Range(2f, 7f));
             rawParticles[i].velocity = new Vector2(0f, 0f);
         }
+
         int stride = Marshal.SizeOf(typeof(FluidParticle));
         particleBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, particleCount, stride); // equivalent to glGenBuffers() and glBindBuffer() in OpenGL
         particleBuffer.SetData(rawParticles); // pushes data to VRAM
+        
         predictedPositionsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, particleCount, Marshal.SizeOf(typeof(Vector2)));
-        hashesBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, particleCount, Marshal.SizeOf(typeof(GPUHash)));
-        neighborBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, particleCount, Marshal.SizeOf(typeof(GPUNeighborData)));
+        lambdaBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, particleCount, Marshal.SizeOf(typeof(float)));
+
+        int totalGridEntries = gridCellCount * maxParticlesPerCell;
+        particlesInCellBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, totalGridEntries, Marshal.SizeOf(typeof(uint)));
+        
+        nInCellBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, gridCellCount, Marshal.SizeOf(typeof(uint)));
 
         vfxGraph.SetGraphicsBuffer("ParticleBuffer", particleBuffer);
         vfxGraph.SetInt("ParticleCount", particleCount);
+        vfxGraph.SetGraphicsBuffer("LambdaBuffer", lambdaBuffer);
     }
 
     void Update() {
         accumulator += Time.deltaTime;
-        int threadGroupsX = Mathf.CeilToInt((float)particleCount / 64f);
+        
+        int threadGroupsParticles = Mathf.CeilToInt((float)particleCount / 64f);
+        int threadGroupsGrid = Mathf.CeilToInt((float)gridCellCount / 64f);
+
         while (accumulator >= fixedDeltaTime) {
+            // send constant parameters to the cbuffer
             pbfShader.SetInt("ParticleCount", particleCount);
-            pbfShader.SetInt("GridCellCount", 2048);
+            pbfShader.SetInt("GridCellCount", gridCellCount);
+            pbfShader.SetInt("MaxParticlesPerCell", maxParticlesPerCell);
             pbfShader.SetFloat("cellSize", cellSize);
             pbfShader.SetFloat("dt", fixedDeltaTime);
             pbfShader.SetFloat("smoothingRadius", smoothingRadius);
             pbfShader.SetVector("gravity", gravity);
 
+            // Predict positions
             pbfShader.SetBuffer(kernelPredict, "Particles", particleBuffer);
             pbfShader.SetBuffer(kernelPredict, "PredictedPositionsBuffer", predictedPositionsBuffer);
-            
-            pbfShader.Dispatch(kernelPredict, threadGroupsX, 1, 1);
+            pbfShader.Dispatch(kernelPredict, threadGroupsParticles, 1, 1);
+
+            // Reset of grid counters (NInCell)
+            pbfShader.SetBuffer(kernelClear, "NInCell", nInCellBuffer);
+            pbfShader.Dispatch(kernelClear, threadGroupsGrid, 1, 1);
+
+            // Fill the spatial grid (InterlockedAdd)
+            pbfShader.SetBuffer(kernelBuild, "PredictedPositionsBuffer", predictedPositionsBuffer);
+            pbfShader.SetBuffer(kernelBuild, "ParticlesInCell", particlesInCellBuffer);
+            pbfShader.SetBuffer(kernelBuild, "NInCell", nInCellBuffer);
+            pbfShader.Dispatch(kernelBuild, threadGroupsParticles, 1, 1);
+
+            // Density and Lambda calculation
+            pbfShader.SetBuffer(kernelDensity, "PredictedPositionsBuffer", predictedPositionsBuffer);
+            pbfShader.SetBuffer(kernelDensity, "ParticlesInCell", particlesInCellBuffer);
+            pbfShader.SetBuffer(kernelDensity, "NInCell", nInCellBuffer);
+            pbfShader.SetBuffer(kernelDensity, "LambdaBuffer", lambdaBuffer);
+            pbfShader.Dispatch(kernelDensity, threadGroupsParticles, 1, 1);
 
             accumulator -= fixedDeltaTime;
         }
@@ -79,7 +117,8 @@ public class FluidBridge : MonoBehaviour
     void OnDestroy() {
         if (particleBuffer != null) { particleBuffer.Release(); particleBuffer = null; }
         if (predictedPositionsBuffer != null) { predictedPositionsBuffer.Release(); predictedPositionsBuffer = null; }
-        if (hashesBuffer != null) { hashesBuffer.Release(); hashesBuffer = null; }
-        if (neighborBuffer != null) { neighborBuffer.Release(); neighborBuffer = null; }
+        if (lambdaBuffer != null) { lambdaBuffer.Release(); lambdaBuffer = null; }
+        if (particlesInCellBuffer != null) { particlesInCellBuffer.Release(); particlesInCellBuffer = null; }
+        if (nInCellBuffer != null) { nInCellBuffer.Release(); nInCellBuffer = null; }
     }
 }
